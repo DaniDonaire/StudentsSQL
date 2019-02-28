7f3a110486533936ec96998f9fdf01d0b0cae8ad
danidonaire <dani.g.donaire@gmail.com>
2019-02-28 13:24:14 +0100

    Start Up Architecture Project. Close #1

---
+++ StudentSQL/StudentSQL.DataAccess.Repository/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSQL.DataAccess.Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
