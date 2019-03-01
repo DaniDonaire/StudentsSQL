037e1470a92dc16b9a2cfee82a69a86d67ac9c9d
danidonaire <dani.g.donaire@gmail.com>
2019-03-01 14:22:23 +0100

    New Models, Close #8

---
+++ StudentSQL/StudentSQL.Common.Library/Models/Direccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSQL.Common.Library.Models
+{
+    public class Direccion
+    {
+        public int DireccionId { get; set; }
+        public string Calle { get; set; }
+        public string Provincia { get; set; }
+        public string Poblacio { get; set; }
+       
+    }
+}
---
+++ StudentSQL/StudentSQL.Common.Library/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSQL.Common.Library.Models
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Dni { get; set; }
+        public int DireccionId { get; set; }
+    }
+}
